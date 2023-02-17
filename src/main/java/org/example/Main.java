de2c4b6f741be480605b2c124286d86336dca311
0310199910031999 <jony.nim@gmail.com>
2023-02-16 21:31:44 -0600

    Se añadieron funcionalidades

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingresa un numero");
+        int num1 = input.nextInt();
+        System.out.println("Ingresa otro numero");
+        int num2 = input.nextInt();
+        int resultado = num1+num2;
+        System.out.println("Resultado: " + resultado);
     }
 }
